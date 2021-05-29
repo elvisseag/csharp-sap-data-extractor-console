d84ffd04e4f2ead2c86274a68b362640bdae4ce3
Elvis Segura <elvisseguraa.05@outlook.com>
2021-05-28 23:05:18 -0500

    Add project files.

---
+++ ConsoleAppSAPRFC/Program.cs
@@ -0,0 +1,166 @@
+﻿using SAP.Middleware.Connector;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSAPRFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictConfig = ReadConfig(@"D:\Cust_SAP_Parameters.ini");
+            //UtilSAP usap = nullll new UtilSAP();
+
+            RfcConfigParameters config = new RfcConfigParameters();
+            config.Add(RfcConfigParameters.Name, dictConfig["Name"]);
+            config.Add(RfcConfigParameters.AppServerHost, dictConfig["AppServerHost"]);
+            config.Add(RfcConfigParameters.SystemNumber, dictConfig["SystemNumber"]);
+            config.Add(RfcConfigParameters.User, dictConfig["User"]);
+            config.Add(RfcConfigParameters.Password, dictConfig["Password"]);
+            config.Add(RfcConfigParameters.Client, dictConfig["Client"]);
+            config.Add(RfcConfigParameters.Language, dictConfig["Language"]);
+
+            RfcDestination destination = RfcDestinationManager.GetDestination(config);
+            RfcRepository repository = null;
+            IRfcFunction functionRFC = null;
+            repository = destination.Repository;
+            functionRFC = repository.CreateFunction("ZES_RFC_SPFLI");
+
+            //PARAMETROS IMPORT
+            
+            functionRFC.SetValue("IS_NAME", "Elvis");
+            functionRFC.SetValue("IS_ROWS", "15");
+
+            IRfcTable rangePersonal = functionRFC.GetTable("IT_COUNTRYTO");
+            rangePersonal.Clear();
+            rangePersonal.Append();//importante ponerlo sino nos lanzara error.
+            rangePersonal.SetValue("SIGN", "I");
+            rangePersonal.SetValue("OPTION", "EQ");
+            rangePersonal.SetValue("LOW", "US");
+            rangePersonal.SetValue("HIGH", "");
+            functionRFC.SetValue("IT_COUNTRYTO", rangePersonal);
+
+            try
+            {
+
+                //INVOKE
+                functionRFC.Invoke(destination);
+
+                //PARAMETROS EXPORT
+
+                string resultado = functionRFC.GetValue("ES_MESSAGE").ToString();
+
+                IRfcTable tData = functionRFC.GetTable("ET_DATA");
+
+                // IRfcStructure mensaje = functionRFC.GetStructure("EW_MENSAJE");
+
+                DataTable dtData = null;
+                dtData = new DataTable();
+                dtData = UtilSAP.toDataTable(tData, "Tabla de Datos");
+                //UploadDataToDB(dtData);
+
+                Console.WriteLine("CAMPO [RESULTADO]: " + resultado);
+                Console.WriteLine("ESTRUCTURA [MENSAJE]: ");
+                //Console.WriteLine("[IND_EXITO] = " + mensaje.GetString("IND_EXITO"));
+                //Console.WriteLine("[MESSAGE] = " + mensaje.GetString("MESSAGE"));
+                Console.WriteLine("TABLA PERSONAL:");
+                foreach (DataRow fila in dtData.Rows)
+                {
+                    // Console.WriteLine("Client: " + fila[0].ToString());
+                    Console.WriteLine("Mandante: " + fila[0].ToString());
+                    Console.WriteLine("Airline Code: " + fila[1].ToString());
+                    Console.WriteLine("Flight Connection Number: " + fila[2].ToString());
+                    Console.WriteLine("Country Key (FROM): " + fila[3].ToString());
+                    Console.WriteLine("Departure city: " + fila[4].ToString());
+                    Console.WriteLine("Country Key (TO): " + fila[6].ToString());
+                    Console.WriteLine("Arrival city: " + fila[7].ToString());
+                    Console.WriteLine("");
+
+                }
+                Console.WriteLine();
+
+                Console.ReadKey();
+
+            }
+            catch (RfcAbapException ex)
+            {
+                if (ex.Key == "CARR_NOT_FOUND")
+                {
+                    Console.WriteLine("Tabla no posee data para esos valores de entrada.");
+                }
+                else
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                    
+            }
+        }
+
+
+        static void UploadDataToDB(DataTable dt)
+        {
+            string strConnString = ConfigurationManager.ConnectionStrings["DBConn"].ToString();
+
+            try
+            {
+                using (SqlBulkCopy sbc = new SqlBulkCopy(strConnString))
+                {
+                    sbc.BatchSize = 10000;
+                    sbc.BulkCopyTimeout = 10000;
+                    //Columnas desde SAP a DB
+                    sbc.ColumnMappings.Add("ROWS_SAP", "ROWS_SQL"); // (campo_sap, campo_db)
+                    sbc.ColumnMappings.Add("NOMBRE_SAP", "NOMBRE_SQL");
+
+                    sbc.DestinationTableName = "TBL_DATA_SQL";// Tabla de BD
+                    sbc.WriteToServer(dt);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+        public static Dictionary<string, string> ReadConfig(string txtFile)
+        {
+            var dict = new Dictionary<string, string>();
+            string line;
+            using (StreamReader sr = new StreamReader(txtFile))
+            {
+                while (!string.IsNullOrEmpty(line = sr.ReadLine()))
+                {
+                    var tmp = line.Split('=');
+                    dict.Add(tmp[0], tmp[1]);
+                }
+
+                return dict;
+            }
+        }
+
+
+
+        //public static string GetValueDict(string key, Dictionary<string, string> dictionary)
+        //{
+        //    string value = "";
+        //    foreach (var item in dictionary)
+        //    {
+        //        if (item.Key == key)
+        //        {
+        //            value = item.Value;
+        //            break;
+        //        }
+        //    }
+        //    return value;
+        //}
+    }
+}
+
---
+++ ConsoleAppSAPRFC/UtilSAP.cs
@@ -0,0 +1,108 @@
+﻿using SAP.Middleware.Connector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSAPRFC
+{
+    public class UtilSAP
+    {
+
+        public static DataTable toDataTable(IRfcTable tabRfc, string name)
+        {
+            DataTable dt = new DataTable(name);
+
+            for (int index = 0; index < tabRfc.ElementCount; index++)
+            {
+                RfcElementMetadata mt = tabRfc.GetElementMetadata(index);
+                dt.Columns.Add(mt.Name, GetDataType(mt.DataType));
+            }
+
+            foreach (IRfcStructure row in tabRfc)
+            {
+                DataRow dr = dt.NewRow();
+                for (int index = 0; index < tabRfc.ElementCount; index++)
+                {
+                    RfcElementMetadata mt2 = tabRfc.GetElementMetadata(index);
+
+                    switch (mt2.DataType)
+                    {
+                        case RfcDataType.DATE://para fecha
+                            String fecha = row.GetString(mt2.Name);
+                            if (!(fecha == "0000-00-00"))
+                            {
+                                dr[mt2.Name] = row.GetString(mt2.Name).Substring(0, 4) + "-" + row.GetString(mt2.Name).Substring(5, 2) + "-" + row.GetString(mt2.Name).Substring(8, 2);
+                            }
+                            break;// la fecha lo pasamos como String.
+                        case RfcDataType.TIME://para hora
+                            String hora = row.GetString(mt2.Name);
+                            if (!(hora == "00:00:00"))
+                            {
+                                dr[mt2.Name] = row.GetString(mt2.Name).Substring(0, 2) + ":" + row.GetString(mt2.Name).Substring(3, 2) + ":" + row.GetString(mt2.Name).Substring(6, 2);
+                            }
+                            break;// la hora lo pasamos como String.
+                        case RfcDataType.BCD://para decimal
+                            dr[mt2.Name] = row.GetDecimal(mt2.Name);//aqui se le asigna el tipo de dato decimal a ese campo de la tabla.
+                            break;
+                        case RfcDataType.CHAR:
+                            dr[mt2.Name] = row.GetString(mt2.Name);
+                            break;
+                        case RfcDataType.STRING:
+                            dr[mt2.Name] = row.GetString(mt2.Name);
+                            break;
+                        case RfcDataType.INT2:
+                            dr[mt2.Name] = row.GetInt(mt2.Name);
+                            break;
+                        case RfcDataType.INT4:
+                            dr[mt2.Name] = row.GetInt(mt2.Name);
+                            break;
+                        case RfcDataType.INT8:
+                            dr[mt2.Name] = row.GetInt(mt2.Name);
+                            break;
+                        case RfcDataType.FLOAT:
+                            dr[mt2.Name] = row.GetDouble(mt2.Name);
+                            break;
+                        default:
+                            dr[mt2.Name] = row.GetString(mt2.Name);
+                            break;
+                    }
+                }
+                dt.Rows.Add(dr);
+            }
+            return dt;
+
+        }//fin de funcion toDataTable
+
+
+        private static Type GetDataType(RfcDataType rfcDataType)
+        {
+            switch (rfcDataType)
+            {
+                case RfcDataType.DATE:
+                    return typeof(string);
+                case RfcDataType.TIME:
+                    return typeof(string);
+                case RfcDataType.CHAR:
+                    return typeof(string);
+                case RfcDataType.STRING:
+                    return typeof(string);
+                case RfcDataType.BCD:
+                    return typeof(decimal);
+                case RfcDataType.INT2:
+                    return typeof(int);
+                case RfcDataType.INT4:
+                    return typeof(int);
+                case RfcDataType.INT8:
+                    return typeof(int);
+                case RfcDataType.FLOAT:
+                    return typeof(double);
+                default:
+                    return typeof(string);
+            }
+        }//fin de funcion GetDataType
+
+    }
+}
